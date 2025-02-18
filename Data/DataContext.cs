ca3dbf804faa5012aeb5c70683006249439bc7d5
Tam <122633077+jjjitmail@users.noreply.github.com>
2025-02-18 12:48:25 +0100

    Add files via upload

---
+++ Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace Data
+{
+    public class DataContext<T> : DbContext where T : class
+    {
+        public DataContext() : base("name=PIMSDB_ZIUTEntities")
+        {
+            //
+        }
+        public DataContext(string databaseConnection) : base(databaseConnection)
+        {
+            //
+        }
+
+        public DbSet<T> Datas { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
